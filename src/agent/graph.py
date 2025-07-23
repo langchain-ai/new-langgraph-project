4a91fdc2010313fd15a8f2450c8531b5cc6ed662
Dudi Radosezki <dudir@moveohls.com>
2025-07-23 11:20:35 +0300

    update graph with llm + update reqs

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -1,54 +1,28 @@
-"""LangGraph single-node graph template.
+from typing import Annotated
 
-Returns a predefined response. Replace logic and configuration as needed.
-"""
+from typing_extensions import TypedDict
 
-from __future__ import annotations
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+import os
+from langchain.chat_models import init_chat_model
 
-from dataclasses import dataclass
-from typing import Any, Dict, TypedDict
+llm = init_chat_model("openai:gpt-4.1")
 
-from langchain_core.runnables import RunnableConfig
-from langgraph.graph import StateGraph
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
 
-
-class Configuration(TypedDict):
-    """Configurable parameters for the agent.
-
-    Set these when creating assistants OR when invoking the graph.
-    See: https://langchain-ai.github.io/langgraph/cloud/how-tos/configuration_cloud/
-    """
-
-    my_configurable_param: str
+graph_builder = StateGraph(State)
 
 
-@dataclass
-class State:
-    """Input state for the agent.
-
-    Defines the initial structure of incoming data.
-    See: https://langchain-ai.github.io/langgraph/concepts/low_level/#state
-    """
-
-    changeme: str = "example"
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
 
 
-async def call_model(state: State, config: RunnableConfig) -> Dict[str, Any]:
-    """Process input and returns output.
-
-    Can use runtime configuration to alter behavior.
-    """
-    configuration = config["configurable"]
-    return {
-        "changeme": "output from call_model. "
-        f'Configured with {configuration.get("my_configurable_param")}'
-    }
-
-
-# Define the graph
-graph = (
-    StateGraph(State, config_schema=Configuration)
-    .add_node(call_model)
-    .add_edge("__start__", "call_model")
-    .compile(name="New Graph")
-)
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile()
